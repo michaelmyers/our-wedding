b7d9d59164683af5e6722572e613da8603f35ad4
Michael Myers <michael.morris.myers@gmail.com>
2017-01-15 18:33:56 -0500

    place holders

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,14 +5,22 @@ import { Col, Row } from "../components/Grid";
 export default class HomePage extends React.Component<any, any> {
     render() {
         return (
-            <Row center>
-                <Col>
-                    <h2>Annie and Michael</h2>
-                    <h4>5:30 PM March 18, 2017</h4>
-                    <h5>Eastern Market</h5>
-                    <h5>Washington, D.C.</h5>
-                </Col>
-            </Row>
+            <span>
+                <Row center>
+                    <Col>
+                        <h2>Annie and Michael</h2>
+                        <h4>5:30 PM March 18, 2017</h4>
+                        <h5>Eastern Market</h5>
+                        <h5>Washington, D.C.</h5>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h4>Schedule</h4>
+                        <h4>Location</h4>
+                    </Col>
+                </Row>
+            </span>
         );
     }
 }
