619b95b41d6ff5c53da7cb5c746350ace30bbd80
Michael Myers <michael.morris.myers@gmail.com>
2017-01-16 22:20:40 -0500

    registry page!

--- src/pages/RegistryPage.tsx
+++ src/pages/RegistryPage.tsx
@@ -1,15 +1,49 @@
 import * as React from "react";
-
+// import { Button } from "react-toolbox/lib/button";
+// import { Card, CardActions, CardMedia } from "react-toolbox/lib/card";
 import { Col, Row } from "../components/Grid";
+
+const style = require("../styles/main.scss");
 
 export default class RegistryPage extends React.Component<any, any> {
     render() {
         return (
-            <Row center>
-                <Col>
-                    <h2>Registry</h2>
-                </Col>
-            </Row>
+            <span>
+                <Row center>
+                    <Col percentage={80}>
+                        <h2>Registry</h2>
+                    </Col>
+                </Row>
+                <Row center>
+                    <Col percentage={60}>
+                        <p style={{ textAlign: "center", fontSize: "1.0em" }}> We understand for some of you it is a destination wedding so your prsecence at our ceremony is the best gift you can give. However if you would like to helps us celebrate with a gift, we thank you for helping us build our home together.</p>
+                    </Col>
+                </Row>
+                <Row center>
+                    <Col percentage={45} style={{ padding: 20 }}>
+                        <a
+                            className={style.link}
+                            href="https://secure.williams-sonoma.com/registry/w9djtcnk2b/access-registry.html"
+                            target="_blank">
+                            <img
+                                style={{ width: "100%" }}
+                                src="http://february20th.com/wp-content/plugins/bean-registry/images/williams-sonoma.png"
+                            />
+                        </a>
+                    </Col>
+                    <Col percentage={45} style={{ padding: 20 }}>
+                        <a
+                            className={style.link}
+                            href="https://secure.williams-sonoma.com/registry/w9djtcnk2b/access-registry.html"
+                            target="_blank">
+                            <img
+                                style={{ width: "100%" }}
+                                src="http://february20th.com/wp-content/plugins/bean-registry/images/crate-and-barrel.png"
+                            />
+                        </a>
+                    </Col>
+                </Row>
+            </span>
         );
     }
 }
