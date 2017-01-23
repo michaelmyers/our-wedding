6130b7bfc46b8ea9220a1d075eeed53051b3ba19
Michael Myers <michael.morris.myers@gmail.com>
2017-01-22 23:08:50 -0500

    updates to area information

--- src/pages/AreaInformationPage.tsx
+++ src/pages/AreaInformationPage.tsx
@@ -16,21 +16,20 @@ export default class AreaInformationPage extends React.Component<any, any> {
                 <Row center>
                     <Col percentage={50}>
                         <h3>Hotels</h3>
-                        <p>We have blocks reserved at two near by hotels.</p>
-                        <p>Since Capitol Hill is mostly residential, we also recommend <a href="https://www.airbnb.com/s/Washington--DC">AirBnB</a></p>
+                        <p>We have blocks reserved at two near by hotels. Since Capitol Hill is mostly residential, we also recommend <a href="https://www.airbnb.com/s/Washington--DC">Airbnb</a></p>
                         <h4>Holiday Inn Express & Suites National Arboretum </h4>
                         <ul className={style.linkList}>
                             <li>1917 Bladensburg Road NE</li>
                             <li>Washington, DC 20002</li>
                             <li>202-266-9000</li>
-                            <li>Ask for the Myers-Rorem wedding.</li>
+                            <li><i>Ask for the Myers-Rorem wedding</i></li>
                         </ul>
                         <h4>Holiday Inn Capitol</h4>
                         <ul className={style.linkList}>
                             <li>550 C Street SW</li>
                             <li>Washington, DC 20024</li>
                             <li>202-479-4000</li>
-                            <li>Ask for the Rorem wedding.</li>
+                            <li><i>Ask for the Rorem wedding.</i></li>
                         </ul>
                     </Col>
                 </Row>
@@ -39,8 +38,9 @@ export default class AreaInformationPage extends React.Component<any, any> {
                         <h3>Transportation</h3>
                         <ul className={style.linkList}>
                             <li><a href="https://www.wmata.com/">Metro</a></li>
-                            <li>Taxi</li>
-                            <li>Uber/Lyft</li>
+                            <li><a href="http://dctaxionline.com/">Taxi</a></li>
+                            <li><a href="https://www.uber.com/">Uber</a>/<a href="https://www.lyft.com/">Lyft</a></li>
+                            <li><a href="https://www.capitalbikeshare.com/">Bikeshare</a></li>
                         </ul>
                     </Col>
                 </Row>
@@ -62,9 +62,12 @@ export default class AreaInformationPage extends React.Component<any, any> {
                 </Row>
                 <Row center>
                     <Col percentage={50}>
-                        <h3>Washington D.C.</h3>
+                        <h3>Explore Washington D.C.</h3>
                         <ul className={style.linkList}>
                             <li><a href="https://www.visitthecapitol.gov/">Tour the U.S. Capitol Building</a></li>
+                            <li><a href="https://www.nps.gov/nama/index.htm">Walk the National Mall</a></li>
+                            <li><a href="http://www.usna.usda.gov/">Visit the National Arboretum</a></li>
+                            <li><a href="http://easternmarket-dc.org/">Explore Eastern Market</a></li>
                         </ul>
                     </Col>
                 </Row>
